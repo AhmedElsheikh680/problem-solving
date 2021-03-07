e86fb9beb65786f77d39a402d8db4364a5cd0b5b
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-07 22:59:35 +0200

    first commit

---
+++ src/main/java/com/problem/solving/Factorial.java
@@ -0,0 +1,15 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        if(num ==1){
+            System.out.println("NO");
+        }else {
+            System.out.println("YES");
+        }
+    }
+}
