c5ca1621d4e38d0362128e4bda807d297083129b
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-09 23:56:20 +0200

    Adding Bear And Big Brother

---
+++ src/main/java/com/problem/solving/BearAndBigBrother.java
@@ -0,0 +1,19 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class BearAndBigBrother {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int value1 = sc.nextInt();
+        int value2 = sc.nextInt();
+        int counter =0;
+        while(value1 <= value2){
+            value1 *=3;
+            value2 *=2;
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
