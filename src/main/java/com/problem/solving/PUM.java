2ab8d998bbdda8ba3fda8ebca2c0c29ff067ca99
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-11 12:47:18 +0200

    Adding PUM

---
+++ src/main/java/com/problem/solving/PUM.java
@@ -0,0 +1,21 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class PUM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int counter=1;
+        for(int i=1; i<=num; i++){
+            if(counter %4 == 0){
+                System.out.println(" PUM");
+                counter++;
+                i--;
+            }else{
+                System.out.print(counter++ + " " +counter++ + " "+ counter++ );
+            }
+        }
+        System.out.println(" PUM");
+    }
+}
---
+++ src/main/java/com/problem/solving/Test.java
@@ -0,0 +1,7 @@
+package com.problem.solving;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
