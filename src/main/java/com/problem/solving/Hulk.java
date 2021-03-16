e08311ee20c049a4ba462f4ecf5ed28d49191fbb
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-16 23:44:49 +0200

    Adding Halk

---
+++ src/main/java/com/problem/solving/Hulk.java
@@ -0,0 +1,27 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        for(int i=1; i<=size;i++){
+            if (i == size) {
+                if(i %2 == 0){
+                    System.out.print("I love it ");
+                }else{
+                    System.out.print("I hate it ");
+                }
+
+            }else {
+                if(i %2 == 0){
+                    System.out.print("I love that ");
+                }else{
+                    System.out.print("I hate that ");
+                }
+            }
+
+        }
+    }
+}
