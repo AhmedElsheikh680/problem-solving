91d2763258e5c132c256b760b82929b2e91d0ecc
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-18 15:40:11 +0200

    Adding WayToLongWords

---
+++ src/main/java/com/problem/solving/WayToLongWords.java
@@ -0,0 +1,18 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class WayToLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        for(int i=0; i<size; i++){
+            String value = sc.next();
+            if(value.toCharArray().length > 10){
+                System.out.println(value.toCharArray()[0] + ""+ (value.toCharArray().length-2)+ ""+ value.toCharArray()[value.length()-1]);
+                continue;
+            }
+            System.out.println(value);
+        }
+    }
+}
