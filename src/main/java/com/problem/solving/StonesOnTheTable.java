4f940d8aa7493cca3b924102f88085ed35c62629
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-28 23:28:53 +0200

    StonesOnTheTable

---
+++ src/main/java/com/problem/solving/StonesOnTheTable.java
@@ -0,0 +1,19 @@
+package com.problem.solving;
+
+import java.util.Scanner;
+
+public class StonesOnTheTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String value = sc.next();
+        char[] values = value.toCharArray();
+        int counter=0;
+        for(int i=0; i<num; i++){
+            if(i !=num-1 && values[i] == values[i+1]){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+}
