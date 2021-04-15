c9613a73f36deae7f8abb09a918bf7495790f5e7
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-15 12:56:24 +0200

    Adding LeftRotationHackerrank

---
+++ src/main/java/com/problem/solving/LeftRotationHackerrank.java
@@ -0,0 +1,29 @@
+package com.problem.solving;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LeftRotationHackerrank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
+        int round = scanner.nextInt();
+        for (int i=0;i<size;i++){
+            arr[i] = scanner.nextInt();
+        }
+        List l = new ArrayList();
+        for (int i=round ;i<size;i++){
+            l.add(arr[i]);
+        }
+        for (int i=0 ;i<round;i++){
+            l.add(arr[i]);
+        }
+        for (int i=0;i<size;i++){
+            System.out.print(l.get(i) + " ");
+        }
+
+    }
+
+}
