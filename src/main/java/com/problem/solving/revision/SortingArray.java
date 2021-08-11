4afe0aa54d43412d6f59efdcbf8a298c0a704859
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-08-11 16:56:21 +0200

    SortingArray

---
+++ src/main/java/com/problem/solving/revision/SortingArray.java
@@ -0,0 +1,40 @@
+package com.problem.solving.revision;
+
+import java.util.Scanner;
+
+public class SortingArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array Size");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        for(int i=0; i<size; i++){
+            System.out.println("Enter Array Elements "+i);
+            arr[i] = sc.nextInt();
+        }
+        // Sorting Array
+        for(int j=0; j<size; j++){
+
+            // get min arr[j] to end of array
+            int min = arr[j];
+            int min_index = j;
+            for(int i=j;i<size;i++){
+                if(arr[i] < min){
+                    min = arr[i];
+                    min_index = i;
+
+                }
+            }
+            // swapping min, arr[min_index]
+            int temp = arr[j];
+            arr[j] = arr[min_index];
+            arr[min_index] = temp;
+        }
+
+
+        for(int i=0; i<size; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
