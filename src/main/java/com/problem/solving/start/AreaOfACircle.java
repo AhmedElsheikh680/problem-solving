3dede68feb279c97acf52041114659214179237d
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-08-14 21:26:34 +0200

    AreaOfACircle

---
+++ src/main/java/com/problem/solving/start/AreaOfACircle.java
@@ -0,0 +1,12 @@
+package com.problem.solving.start;
+
+import java.util.Scanner;
+
+public class AreaOfACircle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float x = sc.nextFloat();
+        System.out.println(3.141592653 * (x*x));
+    }
+}
