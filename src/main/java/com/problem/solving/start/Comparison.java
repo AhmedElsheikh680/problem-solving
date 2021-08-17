326f30bf2fab8c5375f1a03125e81cb7750ad9c4
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-08-17 12:40:01 +0200

    Comparison

---
+++ src/main/java/com/problem/solving/start/Comparison.java
@@ -0,0 +1,32 @@
+package com.problem.solving.start;
+
+import java.util.Scanner;
+
+public class Comparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char s = sc.next().charAt(0);
+        int b = sc.nextInt();
+        if(s == '>'){
+            if(a > b) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        } else if(s == '<') {
+            if(a < b) {
+                System.out.println("Right");
+            }else {
+                System.out.println("Wrong");
+            }
+        }
+        if(s == '=') {
+            if(a == b) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        }
+    }
+}
