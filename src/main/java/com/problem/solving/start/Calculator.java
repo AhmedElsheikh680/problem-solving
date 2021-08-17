b45b7bf324a35beaa67100bde84870dd0de16a9d
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-08-17 08:40:45 +0200

    Interval

--- src/main/java/com/problem/solving/start/Calculator.java
+++ src/main/java/com/problem/solving/start/Calculator.java
@@ -1,18 +1,18 @@
-//package com.problem.solving.start;
-//
-//import java.util.Scanner;
-//
-//public class Calculator {
-//
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int a = sc.nextInt();
-//        char s = sc.nextLine().charAt(0);
-//        int b = sc.nextInt();
-//        if (s=='+') {
-//
-//            System.out.println(a+b);
-//        }
-//
-//    }
-//}
+package com.problem.solving.start;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char s = sc.nextLine().charAt(0);
+        int b = sc.nextInt();
+        if (s=='+') {
+
+            System.out.println(a+b);
+        }
+
+    }
+}
